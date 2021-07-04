d6e1ecc6f4bee879eb2eac47abd6c3586a14cc61
Sgallego <sgallegogirona@gmail.com>
2021-07-04 16:23:33 +0200

    model applicant update

--- models/ApplicantsReview.model.js
+++ models/ApplicantsReview.model.js
@@ -1,36 +1,39 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose')
 
+const applicantsReview = new Schema(
+    {
+        place_name: {
+            type: String,
+            required: true,
+        },
+        host_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        start_date: String,
+        final_date: String,
 
-const applicantsReview = new Schema({
-    placeName: {
-        type: String,
-        required: true
+        direction: {
+            type: String,
+            required: true,
+        },
+        user_applicant: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+
+        cover_letter: String,
+
+        pendingApproved: {
+            type: Boolean,
+            default: false,
+        },
     },
-    host_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    dates: {
-        type: String
-    },
-    direction: {
-        type: String,
-        required: true
-    },
-    user_applicant: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }],
+    { timestamps: true }
+)
 
-    cover_letter: String,
+const Applicants = model('Applicants', applicantsReview)
 
-    pendingApproved: {
-        type: Boolean,
-        default: false
-    }
-
-}, { timestamps: true })
-
-const Applicants = model("Applicants", applicantsReview);
-
-module.exports = Applicants;
+module.exports = Applicants
