5fef9cda7dd01f293b82d4cdd52b762524fdb950
Sgallego <sgallegogirona@gmail.com>
2021-07-06 17:46:19 +0200

    unsplash

--- public/js/unsplash.js
+++ public/js/unsplash.js
@@ -5,8 +5,9 @@ const endPoint = 'https://api.unsplash.com/search/photos'
 // const endPoint = 'https://api.unsplash.com/search/photos'?query=cat&client_id=' + accesKey
 
 async function getImages(query) {
+    const randomNum = Math.trunc(Math.random() * 10 - 1)
     //The code will be here
-    let response = await fetch(`https://api.unsplash.com/search/photos?page=0&per_page=30&query=${query}&client_id=${accesKey}`)
+    let response = await fetch(`https://api.unsplash.com/search/photos?page=${randomNum}&per_page=14&query=${query}&client_id=${accesKey}`)
     let jsonResponse = await response.json()
     let imagesList = await jsonResponse.results
 
@@ -16,12 +17,18 @@ async function getImages(query) {
 function createImages(imagesList) {
     console.log(imagesList)
 
-    for (let i = 0; i < imagesList.length; i++) {
+    for (let i = 0; i < 14; i++) {
         // const image = document.createElement('img')
         image = imagesList[i].urls.thumb
-        let str = `<div class="col-md-3"> <img src="${image}" alt="image"></div>`
-        document.querySelector('.row').insertAdjacentHTML('afterbegin', str)
+        let str = ` <div style="margin:0 0 5px 5px"><img src="${image}" alt="image"></div>`
+        document.querySelector('.home-top').insertAdjacentHTML('afterbegin', str)
     }
+    // for (let i = 7; i < 14; i++) {
+    //     // const image = document.createElement('img')
+    //     image = imagesList[i].urls.thumb
+    //     let str = ` <div style="margin:0 0 5px 5px"><img src="${image}" alt="image"></div>`
+    //     document.querySelector('.home-bottom').insertAdjacentHTML('afterbegin', str)
+    // }
 }
 
-getImages('friend solidary')
+getImages('friends')
