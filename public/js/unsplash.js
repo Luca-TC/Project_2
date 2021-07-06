f94efe91f705f4ec726fd507217382a50bfb9fff
Luca TC <lucatuveri@outlook.it>
2021-07-06 18:36:08 +0200

    homepage style added

--- public/js/unsplash.js
+++ public/js/unsplash.js
@@ -17,18 +17,18 @@ async function getImages(query) {
 function createImages(imagesList) {
     console.log(imagesList)
 
-    for (let i = 0; i < 14; i++) {
+    for (let i = 0; i < 6; i++) {
         // const image = document.createElement('img')
         image = imagesList[i].urls.thumb
-        let str = ` <div style="margin:0 0 5px 5px"><img src="${image}" alt="image"></div>`
+        let str = ` <div ><img class="shadow "  src="${image}" alt="image"></div>`
         document.querySelector('.home-top').insertAdjacentHTML('afterbegin', str)
     }
-    // for (let i = 7; i < 14; i++) {
-    //     // const image = document.createElement('img')
-    //     image = imagesList[i].urls.thumb
-    //     let str = ` <div style="margin:0 0 5px 5px"><img src="${image}" alt="image"></div>`
-    //     document.querySelector('.home-bottom').insertAdjacentHTML('afterbegin', str)
-    // }
+    for (let i = 5; i < 11; i++) {
+        // const image = document.createElement('img')
+        image = imagesList[i].urls.thumb
+        let str = ` <div ><img  class="shadow " src="${image}" alt="image"></div>`
+        document.querySelector('.home-bottom').insertAdjacentHTML('afterbegin', str)
+    }
 }
 
 getImages('friends')
