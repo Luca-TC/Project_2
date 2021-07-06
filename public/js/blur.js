e42a19e7fc5bd49940e718af1d51a93523df2f01
Sgallego <sgallegogirona@gmail.com>
2021-07-06 12:15:41 +0200

    split del blur en otro js

---
+++ public/js/blur.js
@@ -0,0 +1,37 @@
+// blurs
+
+const overlay = document.querySelector('.overlay')
+
+// const modalButton = document.querySelectorAll('.show-modal')
+
+const modal = document.querySelector('.modal')
+const btnClose = document.querySelector('.close-modal')
+
+const closeModal = () => {
+    // modal.classList.add('hidden')
+    rightPan.textContent = ''
+    rightPan.classList.remove('opacity')
+    overlay.classList.add('hidden')
+    rightPan.classList.remove('modal')
+}
+
+// modalButton.forEach(element => {
+//     element.addEventListener('click', openModal)
+// })
+
+// btnClose.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+function openModal() {
+    // modal.classList.remove('hidden')
+
+    rightPan.classList.add('modal')
+    rightPan.classList.add('opacity')
+    overlay.classList.remove('hidden')
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
